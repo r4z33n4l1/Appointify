bd2c6d657b9605fc07de1121ebc6283a54069125
devanshu.singhvi <dev.singhvi@mail.utoronto.ca>
2024-04-10 09:08:34 -0400

    dashboard css update

--- 1on1-front/src/app/dashboard/page.js
+++ 1on1-front/src/app/dashboard/page.js
@@ -8,152 +8,113 @@ import styles from './styles.module.css'; // Adjust the path as necessary
 import { CalendarView} from "@/components/my_calendar.js";
 
 function DashboardLayout() {
-  const { accessToken } = useAuth();
-  const [calendarData, setCalendarData] = useState([]);
-  const [selectedCalendarId, setSelectedCalendarId] = useState('');
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  }
+	const { accessToken } = useAuth();
+	const [calendarData, setCalendarData] = useState([]);
+	const [selectedCalendarId, setSelectedCalendarId] = useState('');
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	}
 
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const authToken = accessToken;
-            const response = await fetch('http://127.0.0.1:8000/calendars/all/', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${authToken}`,
-                },
-            });
-            const responseData = await response.json();
-            setCalendarData(responseData.results);
-            if (responseData.results.length > 0) {
-              setSelectedCalendarId(responseData.results[0].id); // Set default selection to the first calendar
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const authToken = accessToken;
+				const response = await fetch('http://127.0.0.1:8000/calendars/all/', {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${authToken}`,
+					},
+				});
+				const responseData = await response.json();
+				setCalendarData(responseData.results);
+				if (responseData.results.length > 0) {
+					setSelectedCalendarId(responseData.results[0].id); // Set default selection to the first calendar
+				}
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
 
-    fetchData();
-  }, [accessToken]);
+		fetchData();
+	}, [accessToken]);
 
-  const handleCalendarSelectionChange = (event) => {
-    setSelectedCalendarId(event.target.value);
-  };
+	const handleCalendarSelectionChange = (event) => {
+		setSelectedCalendarId(event.target.value);
+	};
 
-  const [calendars, setCalendars] = useState([]);
+	const [calendars, setCalendars] = useState([]);
 
-  useEffect(() => {
-      const fetchCalendars = async () => {
-          const fetchedCalendars = await fetchFinalizedCalendars(accessToken);
-          setCalendars(fetchedCalendars);
-      };
+	useEffect(() => {
+		const fetchCalendars = async () => {
+			const fetchedCalendars = await fetchFinalizedCalendars(accessToken);
+			setCalendars(fetchedCalendars);
+		};
 
-      fetchCalendars();
-  }, [accessToken]);
+		fetchCalendars();
+	}, [accessToken]);
 
-  console.log('calendars', calendars);
+	console.log('calendars', calendars);
 
-  return (
-    <>
-      <NavBar toggleSidebar={toggleSidebar} />
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'left', marginTop: '5vh'}}>
-        <SideBar isSidebarOpen={isSidebarOpen} />
-        <div style={{ marginLeft: '300px', marginTop: '10vh'}}>
-          <a href="/calendar/main_calendar" className="create-calendar-btn" style={{ display: 'inline-flex', alignItems: 'center', color: 'white', textDecoration: 'none' }}>
-              <span style={{ display: 'inline-flex', alignItems: 'flex-start' }}>
-                  <Image src="/assets/plus-512.png" width={20} height={20} alt="Plus Icon" />
-                  <span style={{ margin: '1rem' }}>Create a new calendar</span>
-              </span>
-          </a>
-          <div className={styles.dropdown}>
-            <p>Select Your Calendar</p>
-            <select className={styles.dropdownBtn}onChange={handleCalendarSelectionChange} value={selectedCalendarId}>
-              {calendarData?.map(calendar => (
-                <option key={calendar.id} value={calendar.id}>
-                  {calendar.name}
-                </option>
-              ))}
-            </select>
+return (
+	<>
+	<NavBar toggleSidebar={toggleSidebar} />
+	<div style={{ display: 'd-flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '5vh' }}>
+		<SideBar isSidebarOpen={isSidebarOpen} />
+		<div style={{ marginTop: '10vh'}}></div>
+		<div className={styles.calendarContainer}>
+			<div className={styles.eventslist} style={{maxWidth: '400px', minWidth: '300px'}}>
+			<p style={{color: 'white'}}>Scheduled Events</p>
+				{calendars.length > 0 ? calendars.map(calendar => (
+					<div key={calendar.calendar_id} className={styles.eventcard}>
+						<p>Calendar: {calendar.calendar_name}</p>
+						<p>Date: {calendar.start_time?.split(' ')[0]}</p>
+						<p>Start Time: {calendar.start_time?.split(' ')[1]}</p>
+						<p>End Time: {calendar.end_time?.split(' ')[1]}</p>
+						<p>with {calendar.contact}</p>
+					</div>
+				)) : <p>No events scheduled yet.</p>}
+			</div>
+			<div className={styles.dropdown}>
+				<p>Select Your Calendar</p>
+				<select className={styles.dropdownBtn}onChange={handleCalendarSelectionChange} value={selectedCalendarId}>
+				{calendarData?.map(calendar => (
+					<option key={calendar.id} value={calendar.id}>
+					{calendar.name}
+					</option>
+				))}
+				</select>
 
-          </div>
-          
-        </div>
-        <div className={styles.calendarView}>
-          <CalendarView id={selectedCalendarId} />
-        </div>
+			</div>
+			<CalendarView id={selectedCalendarId} />
+		</div>
 
-        <div class="col-md-6" style={{margin: '10vh'}}>
-        <div class="card">
-            <button class="create-meeting-btn">
-                <Image src="/assets/plus-512.png" width={20} height={20} alt="Plus Icon"/>
-                Create a new meeting
-            </button>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <span class="status-item">
-                        <h5>Approved</h5>
-                        <h6 class="badge">23</h6>
-                    </span>
-                </li>
-                <li class="list-group-item">
-                    <span class="status-item">
-                        <h5>Pending</h5>
-                        <h5 class="badge">19</h5>
-                    </span>
-                </li>
-                <li class="list-group-item">
-                    <span class="status-item">
-                        <h5>Reschedule</h5>
-                        <h5 class="badge">2</h5>
-                    </span>
-                </li>
-                <li class="list-group-item">
-                    <span class="status-item">
-                        <h5>Cancelled</h5>
-                        <h5 class="badge">8</h5>
-                    </span>
-                </li>
-            </ul>
-        </div>
+		<div class="col-md-6" style={{margin: '10vh'}}>
 
-        <div className={styles.eventslist}>
-        <p style={{color: 'white'}}>Scheduled Events</p>
-            {calendars.length > 0 ? calendars.map(calendar => (
-                <div key={calendar.calendar_id} className={styles.eventcard}>
-                    <p>Calendar: {calendar.calendar_name}</p>
-                    <p>Date: {calendar.start_time?.split(' ')[0]}</p>
-                    <p>Start Time: {calendar.start_time?.split(' ')[1]}</p>
-                    <p>End Time: {calendar.end_time?.split(' ')[1]}</p>
-                    <p>with {calendar.contact}</p>
-                </div>
-            )) : <p>Loading events...</p>}
-        </div>
-        </div>
-      </div>
-    </>
-  );
+			
+		</div>
+	</div>
+	</>
+);
 }
 
 
 async function fetchFinalizedCalendars(accessToken) {
-  try {
-      const response = await fetch('http://127.0.0.1:8000/events/finalized_events/', {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${accessToken}`,
-          },
-      });
-      const responseData = await response.json();
-      return responseData.results;
-  } catch (error) {
-      console.error('Error fetching finalized calendars:', error);
-      return [];
-  }
+try {
+	const response = await fetch('http://127.0.0.1:8000/events/finalized_events/', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${accessToken}`,
+		},
+	});
+	const responseData = await response.json();
+	return responseData.results;
+} catch (error) {
+	console.error('Error fetching finalized calendars:', error);
+	return [];
+}
 }
 
 
