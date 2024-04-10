fa9a3dd4ac22be58beaf925cb76c969bdc6988a9
Razeen <razeen_ali@yahoo.com>
2024-04-10 18:37:44 -0400

    hello

--- 1on1-front/next.config.mjs
+++ 1on1-front/next.config.mjs
@@ -1,30 +1,5 @@
-import { withSuperjson } from 'next-superjson';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const publicUrl = 'http://68.183.200.180:8000/';
+export default nextConfig;
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  publicRuntimeConfig: {
-    PUBLIC_URL: publicUrl,
-  },
-  async headers() {
-    return [
-      {
-        // Matching all routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-};
-
-export default withSuperjson(nextConfig);
